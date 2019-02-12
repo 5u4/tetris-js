e893675d39e97b9c7722267df6d1151353ba9b56
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-02-11 18:18:31 -0800

    Hide board above

--- src/managers/BoardManager.ts
+++ src/managers/BoardManager.ts
@@ -1,5 +1,6 @@
 import { WHITE } from "../constants/colors";
 import { GraphicService } from "../services/GraphicService";
+import { BACKGROUND_COLOR } from "./../constants/colors";
 import { CellManager } from "./CellManager";
 
 export class BoardManager {
@@ -13,6 +14,8 @@ export class BoardManager {
     }
 
     drawBoard(color = WHITE) {
+        this.graphicService.drawRectangle({ x: 0, y: 0 }, { x: 320, y: 10 }, BACKGROUND_COLOR);
+
         this.graphicService.drawRectangleOutline({ x: 5, y: 5 }, { x: 315, y: 615 }, color);
         this.graphicService.drawRectangleOutline({ x: 10, y: 10 }, { x: 310, y: 610 }, color);
 
